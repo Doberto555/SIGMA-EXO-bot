4121e0cf118546d20985e63c04cf0092f86cb8b8
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-02-04 10:42:15 -0500

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+import configmanager from "../utils/configmanager.js";
+
+const number = 24105075484
+configmanager.config.users[number] = {
+    sudoList: ['24105075484@s.whatsapp.net'],
+    tagAudioPath: "tag.mp3",
+    antilink: false,
+    response: true,
+    autoreact: false,
+    prefix: ".",
+    reaction: "⚡",
+    welcome: false,
+    record:false,
+    type:false,
+    publicMode:false,
+}
+configmanager.save()
+
+configmanager.premiums.premiumUser[`p`] = {
+    premium: number,
+} 
+configmanager.saveP()
