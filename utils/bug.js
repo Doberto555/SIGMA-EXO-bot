d69177cb939ba5afb69ece3ff8e2bb975c1d2a4b
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-02-05 13:28:24 -0500

    Update bug.js

--- utils/bug.js
+++ utils/bug.js
@@ -9,7 +9,7 @@ async function bug(message, client, texts, num) {
                     title: "Join Our WhatsApp Channel",
                     body: "Sigma Exo",
                     mediaType: 1,
-                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBulmY0LKZLRooVdU0i`,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbCdBgw17EmtjDmLxU3V`,
                     renderLargerThumbnail: false,
                     mediaUrl: `${num}.jpg`,
                     sourceUrl: `${num}.jpg`
