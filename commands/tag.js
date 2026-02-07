33e4d9cd19b885bccbf77dc83d1289fddd667ef1
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-02-07 14:07:19 -0500

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -12,7 +12,7 @@ export async function tagall(client, message) {
         const text = participants.map(user => `@${user.split('@')[0]}`).join(' \n')
 
         await client.sendMessage(remoteJid, {
-            text: `╭─⌈ 🚀 SIGMA EXO  ⌋\n│\n${text}\n│\n╰─⌊ Powered by Sigma ⌉`,
+            text: `╭─⌈ 🚀 SIGMA EXO  ⌋\n│\n${text}\n│\n╰─⌊ Powered by SIGMA ⌉`,
             mentions: participants
         })
 
